8f69d85b29d14d3f5dea3ff8228ae7598f3cea7a
MohamadJ99 <mohamadjawad9991@gmail.com>
2026-08-18 02:07:33 +0300

    feat: add logout api

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,46 +6,60 @@ use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
 
 abstract class Controller
 {
     public function register(RegisterRequest $request)
     {
-        $user=User::create([
-            'name' =>$request->name,
-            'email'=>$request->email,
-            'password'=>Hash::make($request->password)
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
         ]);
 
 
-        $token=$user->createToken('auth_token')->plainTextToken;
+        $token = $user->createToken('auth_token')->plainTextToken;
 
         return response()->json([
-            'message' =>'User registered successfully',
-            'user'=>$user,
-            'token'=>$token
-        ],201);
+            'message' => 'User registered successfully',
+            'user' => $user,
+            'token' => $token
+        ], 201);
     }
 
-   public function login(LoginRequest $request)
-   {
-    $user=User::where('email',$request->email)->first();
+    public function login(LoginRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
 
-    if(!$user || !Hash::check($request->password,$user->password))
-        {
+        if (!$user || !Hash::check($request->password, $user->password)) {
             return response()->json([
-                'message'=>'The email or password is incorrect.'
-            ],401);
+                'message' => 'The email or password is incorrect.'
+            ], 401);
         }
- 
-    $token=$user->createToken('auth_token')->plainTextToken;
 
-     return response()->json([
-        'message'=>'Login success',
-        'user'=>$user,
-        'token'=>$token
-     ]);
+        $token = $user->createToken('auth_token')->plainTextToken;
 
-   }
+        return response()->json([
+            'message' => 'Login success',
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
 
+    public function user(Request $request)
+    {
+        return response()->json([
+            'user' => $request->user(),
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Logged out successfully',
+        ]);
+    }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->redirectGuestsTo(function (Request $request) {
+        return null;
+    });
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(

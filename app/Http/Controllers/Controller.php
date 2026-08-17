707a80cee95d75f075974986590014cf0e0bdd9e
MohamadJ99 <mohamadjawad9991@gmail.com>
2026-08-18 00:08:45 +0300

    feat: add user registration api

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
 abstract class Controller
 {
-    //
+    public function register(RegisterRequest $request)
+    {
+        $user=User::create([
+            'name' =>$request->name,
+            'email'=>$request->email,
+            'password'=>Hash::make($request->password)
+        ]);
+
+
+        $token=$user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' =>'User registered successfully',
+            'user'=>$user,
+            'token'=>$token
+        ],201);
+    }
 }

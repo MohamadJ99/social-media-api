94d97a2112d95270fa66ffc2d6efe91ba051a943
MohamadJ99 <mohamadjawad9991@gmail.com>
2026-08-18 01:46:35 +0300

    feat: add login api

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
@@ -25,4 +26,26 @@ abstract class Controller
             'token'=>$token
         ],201);
     }
+
+   public function login(LoginRequest $request)
+   {
+    $user=User::where('email',$request->email)->first();
+
+    if(!$user || !Hash::check($request->password,$user->password))
+        {
+            return response()->json([
+                'message'=>'The email or password is incorrect.'
+            ],401);
+        }
+ 
+    $token=$user->createToken('auth_token')->plainTextToken;
+
+     return response()->json([
+        'message'=>'Login success',
+        'user'=>$user,
+        'token'=>$token
+     ]);
+
+   }
+
 }
